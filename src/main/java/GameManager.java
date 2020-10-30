492f212538e27dbba2d5a38266bb8fbc65d6b3f1
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-10-30 17:35:04 +0100

    Initial commit

---
+++ src/main/java/GameManager.java
@@ -0,0 +1,10 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GameManager extends JavaPlugin 
+{
+	@Override
+	public void onEnable()
+	{
+		getLogger().info("Game manager loaded.");
+	}
+}
